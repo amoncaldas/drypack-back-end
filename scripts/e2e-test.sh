a24da2d3a4664b55d0d89bbf24c4f153900996ec
Amon <amoncaldas@yahoo.com.br>
2018-03-04 21:03:04 -0300

    Implemented e2e in dev docker

--- scripts/e2e-test.sh
+++ scripts/e2e-test.sh
@@ -5,10 +5,13 @@ APP_ENV=testing php artisan migrate:reset --quiet
 APP_ENV=testing php artisan migrate --seed --quiet
 
 # Starting the server in test mode
-( APP_ENV=testing php artisan serve --quiet -n --port=5020 --host=0.0.0.0 --no-ansi & ) > /dev/null 2>&1
+( APP_ENV=testing php artisan serve --quiet -n --port=5000 --host=0.0.0.0 --no-ansi & ) > /dev/null 2>&1
 
 # Running the tests in the front-end
 protractor public/admin/tests/conf.js $1
 
 # Kill the server process
-lsof -t -i tcp:5020 | xargs kill -9
+lsof -t -i tcp:5000 | xargs kill -9
+
+# Remove temp files
+rm -rf .org.chromium.Chromium.*
