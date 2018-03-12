e212a39e3b7433d1907b492c13a609d6bf14607d
Amon <amoncaldas@yahoo.com.br>
2018-03-12 07:43:04 -0300

    Added command to-container

--- scripts/e2e-test.sh
+++ scripts/e2e-test.sh
@@ -7,6 +7,8 @@ APP_ENV=testing php artisan migrate --seed --quiet
 # Starting the server in test mode
 ( APP_ENV=testing php artisan serve --quiet -n --port=5000 --host=0.0.0.0 --no-ansi & ) > /dev/null 2>&1
 
+# You also have to run, manually, in another tab: webdriver-manager start
+
 # Running the tests in the front-end
 protractor public/admin/tests/conf.js $1
 
